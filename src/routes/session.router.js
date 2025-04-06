27977e73da3b5aa0a84d4152c57576e0255699f8
vag <victoragustingutierrez@gmail.com>
2025-04-06 02:16:57 -0300

    40commit

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -7,21 +7,22 @@ const router = express.Router();
 
 const JWT_SECRET = "secretoJWT123";
 
-// Ruta de login básica
+
 router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-  
-    const user = await UserModel.findOne({ email });
-  
-    if (!user || user.password !== password) { // ¡Reemplazá con hash real en producción!
-      return res.status(401).json({ error: "Credenciales inválidas" });
-    }
-  
-    const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET, { expiresIn: "1h" });
-  
-    res.json({ token });
-  });
-// Estrategia "current"
+  const { email, password } = req.body;
+
+  const user = await UserModel.findOne({ email });
+
+  if (!user || user.password !== password) { // ¡Reemplazá con hash real en producción!
+    return res.status(401).json({ error: "Credenciales inválidas" });
+  }
+
+  const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET, { expiresIn: "1h" });
+
+  res.json({ token });
+});
+
+
 router.get("/current", passport.authenticate('jwt', { session: false }), (req, res) => {
   const safeUser = new UserDTO(req.user);
   res.json({ user: safeUser });
